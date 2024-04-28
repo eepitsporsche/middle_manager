df026fba3f32a9697586a0a1732a32d8df044e2c
Porsche <herskorn@gmail.com>
2024-04-28 15:50:42 -0700

    added "addEmployee" function to server.js

--- server.js
+++ server.js
@@ -68,10 +68,82 @@ function viewAllEmplyees() {
         //Pull Data from Employee Table and Attach Respective Manager Data
         'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee AS manager ON employee.manager_id = manager_id',
             (err, res) => {
-                //Throw Error Otherwise, Display Response Results
+                //Display Response Results or Throw Error
                 if (err) throw err;
                 console.table(res);
                 appInit();
             }
     )
+};
+
+
+//Add Employee Function
+function addEmployee() {
+    //Pull Role Data from Database
+    mysqlConnection.query('SELECT * FROM role', (err, role) => {
+        if (err) throw err;
+
+    //Pull Employee Data from Database
+    mysqlConnection.query('SELECT * FROM employee', (err, employee) => {
+        if (err) throw err;
+
+        //Inquirer Prompts to Add New Employee Data
+        inquirer
+            .prompt([
+                {
+                    name: "firstName",
+                    type: "input",
+                    message: "What is the employee's first name?"
+                },
+
+                {
+                    name: "lastName",
+                    type: "input",
+                    message: "What is the employee's last name?"
+                },
+
+                {
+                    name: "role",
+                    type: "list",
+                    message: "What is the employee's role?",
+                    choices: role.map((role) => role.title)
+                },
+
+                {
+                    name: "manager",
+                    type: "list",
+                    message: "Who is the employee's manager?",
+                    choices: [
+                        "None",
+                        ...employee.map((employee) => `${employee.first_name} ${employee.last_name}`),
+                    ]
+                }
+            ])
+        
+            .then((answers) => {
+                //Pull Role Data from Role Table
+                const selectedRole = role.find((role) => role.title === answers.role);
+                
+                //If a Manager is Selected, Pull ID Data from Employee Table for Manager ID
+                let managerID = null;
+                if (answers.manager !== "None") {
+                    const selectedManager = employee.find((employee) => `${employee.first_name} ${employee.last_name}` === answers.manager)
+                managerID = selectedManager.id;
+                }
+                //Add New Employee Data to Employee Table Via MySql Query
+                mysqlConnection.query("INSERT INTO employee SET ?", {
+                    first_name: answers.firstName,
+                    last_name: answers.lastName,
+                    role_id: selectedRole.id,
+                    manager_id: managerID,
+                },
+                //Display Confirmation Message or Throw Error
+                (err) => {
+                    if (err) throw err;
+                    console.log("New employee added.");
+                    appInit();
+                })
+            })
+    })
+    })
 };
